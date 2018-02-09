9e18b33d405a0ea1e86db5e05244f1dbc20d6bad
Tim McIver <tim@timmciver.com>
2018-02-09 15:25:42 -0500

    Using Scotty to serve up data blobs.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Main where
 
+import Web.Scotty
+import Network.HTTP.Types (status404)
+import qualified RainbowHash as RH
+import Data.Maybe (maybe)
+import Control.Monad.IO.Class
+import qualified Data.ByteString.Lazy as LB
+
 main :: IO ()
-main = putStrLn "Hello world!"
+main = scotty 3000 $ do
+  get "/blob/:hash" $ do
+    h <- param "hash"
+    dataMaybe <- liftIO $ RH.get h
+    let strictDataMaybe = LB.fromStrict <$> dataMaybe
+    maybe nouFound' raw strictDataMaybe
+    where nouFound' :: ActionM ()
+          nouFound' = status status404
