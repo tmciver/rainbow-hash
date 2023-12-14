845553432ab43ec4e4dde912f41c9ee766d0a99d
Tim McIver <tim@timmciver.com>
2023-12-14 13:45:33 -0500

    CLI command to upload file or dir (#6)
    * Add ability to upload a single file from cli.
    * Add ability to upload all files in a given directory.

--- cli/src/RainbowHash/CLI.hs
+++ cli/src/RainbowHash/CLI.hs
@@ -12,23 +12,36 @@ import Protolude
 
 import System.FSNotify
 import qualified Data.Text as T
+import System.Directory (doesDirectoryExist, listDirectory, doesFileExist)
 
 import RainbowHash.CLI.Config (Config)
 import RainbowHash.HttpClient (postFile, runHttpClient)
 
-newtype Command
+import System.FilePath ((</>))
+
+data Command
   = WatchDir FilePath
+  | UploadFile FilePath
+  | UploadDir FilePath
 
 getCommand :: IO (Either Text Command)
 getCommand = do
   args <- getArgs
-  pure $ case args of
-    "watch":rest -> case rest of
+  case args of
+    "watch":rest -> pure $ case rest of
       [dir] -> Right $ WatchDir dir
       [] -> Left "You must supply a path to a directory for the 'watch' command."
       _ -> Left "Too many arguments for the 'watch' command."
-    cmdStr:_ -> Left $ "Unrecognized command " <> T.pack cmdStr
-    [] -> Left "You must supply a command."
+    "upload":rest -> case rest of
+      [fileOrDirectory] -> do
+        isDir <- doesDirectoryExist fileOrDirectory
+        pure . Right $ if isDir
+          then UploadDir fileOrDirectory
+          else UploadFile fileOrDirectory
+      [] -> pure . Left $ "You must supply a path to a file for the 'upload' command."
+      _ -> pure . Left $ "Too many arguments for the 'upload' command."
+    cmdStr:_ -> pure . Left $ "Unrecognized command " <> T.pack cmdStr
+    [] -> pure . Left $ "You must supply a command."
 
 runCommand
   :: ( MonadReader Config m
@@ -37,6 +50,12 @@ runCommand
   => Command
   -> m ()
 runCommand (WatchDir dir) = watchDirectory dir
+runCommand (UploadFile file) = putFile file
+runCommand (UploadDir dir) = do
+  files <- liftIO $ listDirectory dir
+    <&> fmap (dir </>)
+    >>= filterM doesFileExist
+  traverse_ putFile files
 
 watchDirectory
   :: ( MonadReader Config m
@@ -60,6 +79,15 @@ watchDirectory fp = do
     where isFileAdded :: Event -> Bool
           isFileAdded Added{} = True
           isFileAdded _ = False
+
+putFile
+  :: ( MonadReader Config m
+     , MonadIO m
+     )
+  => FilePath
+  -> m ()
+putFile fp =
+  ask >>= liftIO . runHttpClient (postFile fp)
 
 uploadAction :: Config -> Action
 uploadAction config (Added fp _ False) = runHttpClient (postFile fp) config
