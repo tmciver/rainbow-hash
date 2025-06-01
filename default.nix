b6928339020e147a8075e07c979cabf270513de9
Tim McIver <tim@timmciver.com>
2025-06-01 16:36:40 -0400

    Switch to using developPackage.

--- default.nix
+++ default.nix
@@ -1,21 +1,29 @@
-{ mkDerivation,
-  base, bytestring, lib, mtl, protolude, text, url, aeson, blaze-html, data-default,
-  fsnotify, hashing, http-client, http-types, lens, magic, modern-uri,
-  monad-logger, nonempty-containers, ordered-containers, scotty, split, temporary, wai-extra,
-  wreq, yaml, optparse-applicative
-}:
-mkDerivation {
-  pname = "rainbow-hash";
-  version = "0.1.0.0";
-  src = ./.;
-  isLibrary = false;
-  isExecutable = true;
-  executableHaskellDepends = [
-    base bytestring mtl protolude text aeson blaze-html data-default
-    fsnotify hashing http-client http-types lens magic modern-uri
-    monad-logger nonempty-containers ordered-containers scotty split
-    temporary wai-extra wreq yaml optparse-applicative
-  ];
-  license = "unknown";
-  hydraPlatforms = lib.platforms.none;
+{ pkgs ? import ./nixpkgs-pinned.nix }:
+
+pkgs.haskellPackages.developPackage {
+  root = ./.;
 }
+
+
+
+# { mkDerivation,
+#   base, bytestring, lib, mtl, protolude, text, url, aeson, blaze-html, data-default,
+#   fsnotify, hashing, http-client, http-types, lens, magic, modern-uri,
+#   monad-logger, nonempty-containers, ordered-containers, scotty, split, temporary, wai-extra,
+#   wreq, yaml, optparse-applicative
+# }:
+# mkDerivation {
+#   pname = "rainbow-hash";
+#   version = "0.1.0.0";
+#   src = ./.;
+#   isLibrary = false;
+#   isExecutable = true;
+#   executableHaskellDepends = [
+#     base bytestring mtl protolude text aeson blaze-html data-default
+#     fsnotify hashing http-client http-types lens magic modern-uri
+#     monad-logger nonempty-containers ordered-containers scotty split
+#     temporary wai-extra wreq yaml optparse-applicative
+#   ];
+#   license = "unknown";
+#   hydraPlatforms = lib.platforms.none;
+# }
